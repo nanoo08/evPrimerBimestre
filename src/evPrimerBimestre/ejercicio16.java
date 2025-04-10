abe3b9b64c9e546bf3a265906d1b0bd05c6320a3
Your Name <you@example.com>
2025-04-10 10:19:09 -0300

    evLisandrocequeira

---
+++ src/evPrimerBimestre/ejercicio16.java
@@ -0,0 +1,43 @@
+package evPrimerBimestre;
+
+import java.util.Scanner;
+
+public class ejercicio16 {
+
+	public static void main(String[] args) {
+		Scanner ingreso = new Scanner (System.in);
+	
+		int variable;
+		
+		System.out.println("Ingrese un numero dependiendo el cafe que quiera:");
+		variable = ingreso.nextInt();
+		
+		switch (variable) {
+		
+		case 1 :System.out.println("cafe cortado");
+		break;
+		
+		case 2 :System.out.println("cafe latte");
+		break;
+		
+		case 3 :System.out.println("cafe solo");
+		break;
+		
+		case 4 :System.out.println("cafe lagrima");
+		break;
+		
+		case 5 :System.out.println("salio del programa");
+		break;
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+}		
+}
+}
---
+++ src/evPrimerBimestreLisandroCequeira/ejericicio13.java
@@ -0,0 +1,35 @@
+package evPrimerBimestreLisandroCequeira;
+
+import java.util.Scanner;
+
+public class ejericicio13 {
+	
+	public static void main(String[] args) {
+		Scanner ingreso = new Scanner (System.in);
+	
+	int horaIngresada;
+	int minutoIngresado;
+	int segundoIngresado;
+	
+	System.out.println("ingrese la hora: ");
+	horaIngresada = ingreso.nextInt();
+	
+	System.out.println("ingrese el minuto: ");
+	minutoIngresado= ingreso.nextInt();
+	
+	System.out.println("ingrese el segundo: ");
+	segundoIngresado= ingreso.nextInt();
+	
+
+	if (horaIngresada) {
+        System.out.println("hora correcta"); 
+        
+        
+	}
+	 
+	  
+         
+       
+
+}
+}
